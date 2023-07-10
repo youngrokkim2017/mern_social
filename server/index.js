cfd6fb0eb714e05d987673603b117e75e7887e81
Patrick Kim <patrick.yrkim@gmail.com>
2023-07-10 11:35:55 -0700

    Fix syntax error

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ const upload = multer({ storage })
 // mongodb
 const PORT = process.env.PORT || 6001
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParse: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
