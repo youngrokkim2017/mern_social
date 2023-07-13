dc62e5b77a4979a213c3b3ca57717e714872a1bd
Patrick Kim <patrick.yrkim@gmail.com>
2023-07-12 23:48:46 -0700

    Add post routes

--- server/index.js
+++ server/index.js
@@ -10,8 +10,11 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 
 import { register } from './controllers/auth.js'
+import { createPost } from './controllers/posts.js'
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
+import { verifyToken } from './middleware/auth.js'
 
 // configurations
 // middleware
@@ -51,7 +54,9 @@ mongoose.connect(process.env.MONGO_URL, {
 
 // ROUTES FOR UPLOAD
 app.post("/auth/register", upload.single("picture"), register)
+app.post("/posts", verifyToken, upload.single("picture"), createPost)
 
 // ROUTES
 app.use("/auth", authRoutes)
 app.use("/users", userRoutes)
+app.use("/posts", postRoutes)
